a8374c364edcd9d908fbb7a15a4e17f410e1cd2f
gatotoadrian <gatotoadrian@gmail.com>
2022-07-07 10:54:53 +0300

    modified files

--- PickBook/settings.py
+++ PickBook/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-6@6d7__jdyao&q%iqd=td^2qa(+-accymt1+v5px0sp=$5e^vs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
