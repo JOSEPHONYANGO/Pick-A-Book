71e84ca04d3dcd535dfad3307d5e7cfa41b6f2b7
Samuels <100128930+Wanjiru393@users.noreply.github.com>
2022-07-08 11:08:32 +0300

    Revert "Revert "Revert "ft:Cart and Delivery API"""

--- Book/admin.py
+++ Book/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-from .models import Profile, Books,Category,Cart,Delivery, Payment
-
+from .models import Profile, Books,Category,Payment
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Books)
 admin.site.register(Category)
-admin.site.register(Cart)
-admin.site.register(Delivery)
 admin.site.register(Payment)
 
 
